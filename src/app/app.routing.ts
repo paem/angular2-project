0211e2904d7ae718ca54b705a996cb824b46b0a0
Patrik Emilsson <patrik_emilsson@hotmail.com>
2017-03-01 19:48:48 +0100

    routing and english text

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,7 +14,7 @@ const APP_ROUTES: Routes = [
   { path: 'about', component: AboutPageComponent, canActivate: [AuthGuard]},
   { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
   { path: 'mystable', component: MyStablePageComponent, canActivate: [AuthGuard] },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '',   redirectTo: '/overview', pathMatch: 'full' },
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
