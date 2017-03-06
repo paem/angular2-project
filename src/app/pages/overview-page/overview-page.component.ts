26615755cb46180f51059e1e72b55741e16e8a52
Rasmus Josefsson <josefssonrasmus@hotmail.se>
2017-03-06 23:31:15 +0100

    fixed charts to show correct data

--- src/app/pages/overview-page/overview-page.component.ts
+++ src/app/pages/overview-page/overview-page.component.ts
@@ -21,9 +21,7 @@ export class OverviewPageComponent implements OnInit {
 
 
   isLoading = true;
-
   currentHorseTrainings = [];
-  demo = [];
   myPonnys = [];
 
   constructor(private angularfire: AngularFire) {
@@ -44,22 +42,10 @@ export class OverviewPageComponent implements OnInit {
   }
 
 
-  /********************************************************
-   hämta hem hästarna på: .../v1/horses/ + horse.key
-   Varje häst har sedan träningar på sig
-   som ni loopar över och hämtar ner från /v1/trainings/ + training.key
-   ---------------------------------------------------------
-   Example:  [horses] -> [horse, horse] -> [horseName, [trainings]]
-   ---------------------------------------------------------
-   ********************************************************/
-
-
-
-
-
   // Get the horses and push them to this.myPonnys
   // and get each horse training.
   getHorses() {
+
 
     this.horses = this.angularfire.database.list('/v1/userinfo/' + this.userKey + '/horses/')
       .map(horses => {
@@ -71,20 +57,11 @@ export class OverviewPageComponent implements OnInit {
 
 
     this.horses.subscribe(horses => {
-
       // foreach horse
       horses.map(horse => {
 
-        console.log(horse);
-
         // subscribe on current horse in loop to get data.
         horse.data.subscribe(myHorse => {
-
-          // horse data
-          console.log("HHH", myHorse);
-
-          this.myPonnys.push(myHorse);
-
 
           // Get trainings of the horse
           this.getTrainings(myHorse);
@@ -95,26 +72,36 @@ export class OverviewPageComponent implements OnInit {
   }
 
 
-
   barChartData: any[] = [];
+
+  horseAndTrainings: Observable<any[]>;
 
   // Get the horse trainings
   getTrainings(myhorse) {
+
+
     for (let trainingKey in myhorse.trainings) {
-      this.horseTrainings = this.angularfire.database.object('/v1/trainings/' + trainingKey).map(trainings => {
-          return trainings;
-        }
-      );
 
 
-      this.horseTrainings.subscribe((training: any) => {
-        this.currentHorseTrainings.push(training);
-        let data = [training.canter.time, training.trot.time, training.walk.time];
-        this.barChartData.push([ {data: data, label: 'Test Test'}]);
-        this.isLoading = false;
+      this.horseAndTrainings = this.angularfire.database.object('/v1/trainings/' + trainingKey)
+        .map(training => {
+            return training;
+          }
+        );
+
+
+      myhorse.trainingsData = [];
+
+      this.horseAndTrainings.subscribe((training: any) => {
+        myhorse.trainingsData.push(training);
       });
 
     }
+
+
+    this.myPonnys.push(myhorse);
+    this.isLoading = false;
+
   }
 
 
@@ -123,45 +110,14 @@ export class OverviewPageComponent implements OnInit {
 
 
 
+  /*
 
+   this.currentHorseTrainings.push(training);
+   let data = [training.canter.time, training.trot.time, training.walk.time];
+   this.barChartData.push([{data: data, label: 'Test'}]);
+   this.isLoading = false;
 
-
-
-  trainingsInfo() {
-
-    let userHorses = this.angularfire.database.list('/v1/userinfo/' + this.userKey + '/horses/')
-      .map(horses => {
-        horses.map(horse => {
-          horse.data = this.angularfire.database.object('/v1/horses/' + horse.$key);
-        });
-        return horses;
-      });
-
-    userHorses.subscribe(horses => {
-      horses.map(horse => {
-        horse.data.subscribe(data => {
-
-          console.log(data);
-          console.log(data.trainings);
-
-
-          for (let trainingKey in data.trainings) {
-            this.horseTrainings = this.angularfire.database.list('/v1/trainings/' + trainingKey);
-          }
-
-
-          this.horseTrainings.subscribe(trainings => {
-            console.log(data.$key);
-            console.log(trainings);
-
-            this.demo.push({horseKey: data.$key, data: data, trainings: trainings})
-            console.log("custom horse array", this.demo);
-          });
-
-        });
-      });
-    });
-  }
+   */
 
 
   private list = [
@@ -217,7 +173,6 @@ export class OverviewPageComponent implements OnInit {
 
 
 
-
   public barChartOptions: any = {
     responsive: true,
     barThickness: 100
@@ -225,7 +180,11 @@ export class OverviewPageComponent implements OnInit {
 
   };
 
-  public barChartColors:Array<any> = [
+  public barChartLabels: string[] = ['canter', 'trot', 'walk'];
+  public barChartType: string = 'bar';
+  public barChartLegend: boolean = true;
+
+  public barChartColors: Array<any> = [
     {
       backgroundColor: '#ff724f',
       borderColor: '#000000',
@@ -238,13 +197,6 @@ export class OverviewPageComponent implements OnInit {
 
 
   ];
-
-  public barChartLabels: string[] = ['canter','trot', 'walk'];
-
-
-  public barChartType: string = 'bar';
-
-  public barChartLegend: boolean = true;
 
 
 }
